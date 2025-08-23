fc65349ab2fc456a386af454fec47427fed59523
Camyro - C1Studios <c1studioscontato@gmail.com>
2025-08-23 03:27:55 +0000

    3.9

--- ultra/manuais/view/view.js
+++ ultra/manuais/view/view.js
@@ -7,7 +7,7 @@ function findTransactions() {
     // Verifica se o Firebase está inicializado e se firestore está disponível
     if (typeof firebase === 'undefined' || !firebase.firestore) {
         console.error('Firebase ou Firestore não inicializado.');
-        addTransactionsToScreen([{site: 'Erro: Firebase não inicializado'}]);
+        addTransactionsToScreen([{ site: 'Erro: Firebase não inicializado' }]);
         return;
     }
 
@@ -16,7 +16,7 @@ function findTransactions() {
         .get()
         .then(snapshot => {
             if (snapshot.empty) {
-                addTransactionsToScreen([{site: 'Nenhum dado encontrado'}]);
+                addTransactionsToScreen([{ site: 'Nenhum dado encontrado' }]);
                 return;
             }
             const teste = snapshot.docs.map(doc => doc.data());
@@ -24,7 +24,7 @@ function findTransactions() {
         })
         .catch(error => {
             console.error('Erro ao buscar dados:', error);
-            addTransactionsToScreen([{site: 'Erro ao carregar dados: ' + error.message}]);
+            addTransactionsToScreen([{ site: 'Erro ao carregar dados: ' + error.message }]);
         });
 }
 
@@ -33,11 +33,12 @@ function addTransactionsToScreen(teste) {
     const urlParams = new URLSearchParams(window.location.search);
     const siteParam = urlParams.get('const');
     console.info("const is " + siteParam);
-    
+
     var element = document.getElementById('element');
     if (element && teste.length > 0) {
         const propertyName = siteParam; // Se siteParam estiver presente, use isso, caso contrário, use 'site'
         const siteValue = teste[0][propertyName] || 'Dados não encontrados'; // Acessa a propriedade dinamicamente
+        document.getElementById("title").innerHTML = propertyName + " - View Ultra" || 'Erro - View Ultra';
         element.innerHTML = siteValue;
     } else if (!element) {
         console.error('Elemento com id "element" não encontrado no DOM.');
